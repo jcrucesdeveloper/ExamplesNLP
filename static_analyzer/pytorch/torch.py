ef750ca630de9f7e56ad1c78a9ce78054ef8fee3
Jorge Cruces <jcrucesdeveloper@gmail.com>
2024-12-04 21:52:49 -0300

    feat: documentation tab added

---
+++ static_analyzer/pytorch/torch.py
@@ -0,0 +1,67 @@
+from operator import Operator
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#creation-ops
+CREATION_OPERATIONS = [
+    Operator("tensor", ["data"], "output: Tensor"),
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#indexing-slicing-joining-mutating-ops
+INDEXING_SLICING_JOINING_MUTATING_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#accelerators
+ACCELERATOR_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#random-sampling
+RANDOM_SAMPLING_OPERATIONS = [
+    
+]
+
+# Documentationhttps://pytorch.org/docs/stable/torch.html#serialization
+SERIALIZATION_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#parallelism
+PARALLELISM_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#locally-disabling-gradient-computation
+LOCALLY_DISABLING_GRADIENT_COMPUTATION_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#math-operations
+MATH_OPERATIONS = [
+    
+]
+
+# Documentation: https://pytorch.org/docs/stable/torch.html#utilities
+UTILITIES_OPERATIONS = [
+    
+]
+
+PYTORCH_OPERATIONS = [
+    CREATION_OPERATIONS,
+    INDEXING_SLICING_JOINING_MUTATING_OPERATIONS,
+    ACCELERATOR_OPERATIONS,
+    RANDOM_SAMPLING_OPERATIONS,
+    SERIALIZATION_OPERATIONS,
+    PARALLELISM_OPERATIONS,
+    LOCALLY_DISABLING_GRADIENT_COMPUTATION_OPERATIONS,
+    MATH_OPERATIONS,
+    UTILITIES_OPERATIONS,
+    MATH_OPERATIONS,
+    UTILITIES_OPERATIONS,
+]
+
+
+
+
+
+
+
